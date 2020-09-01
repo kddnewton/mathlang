5f212e89d634bfdfd578260563ea602b185061d3
Kevin Deisz <kevin.deisz@gmail.com>
2020-09-01 11:08:40 -0400

    stdlib maybe

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
 type AST = (
-  | { type: "binary", left: AST, oper: string, right: AST }
   | { type: "getLocal", name: string }
   | { type: "number", value: number }
+  | { type: "send", recv: AST, name: string, args: AST[] }
   | { type: "setLocal", name: string, value: AST }
   | { type: "statements", stmts: AST[] }
 )
 
-const binary = (left: AST, oper: string, right: AST): AST => ({ type: "binary", left, oper, right });
 const getLocal = (name: string): AST => ({ type: "getLocal", name });
 const number = (value: number): AST => ({ type: "number", value });
+const send = (recv: AST, name: string, args: AST[]) => ({ type: "send" as const, recv, name, args });
 const setLocal = (name: string, value: AST): AST => ({ type: "setLocal", name, value });
 const statements = (stmts: AST[]): AST => ({ type: "statements", stmts });
 
-const add = (left: AST, right: AST): AST => binary(left, "+", right);
-const subtract = (left: AST, right: AST): AST => binary(left, "-", right);
+const add = (left: AST, right: AST): AST => send(left, "+", [right]);
+const negative = (value: AST): AST => send(value, "@-", []);
+const subtract = (left: AST, right: AST): AST => send(left, "-", [right]);
 
 const debug = (node: AST): string => {
   switch (node.type) {
-    case "binary":
-      return `binary(${debug(node.left)}, ${node.oper}, ${debug(node.right)})`;
     case "getLocal":
       return `getLocal(${node.name})`;
     case "number":
-      return node.value.toString();
+      return `number<${node.value}>`;
+    case "send":
+      return `${debug(node.recv)}.${node.name}(${node.args.map(debug).join(", ")})`;
     case "setLocal":
       return `setLocal(${node.name}, ${debug(node.value)})`;
     case "statements":
@@ -30,56 +31,43 @@ const debug = (node: AST): string => {
   }
 };
 
-const ruby = (node: AST): string => {
-  switch (node.type) {
-    case "binary":
-      return `${ruby(node.left)} ${node.oper} ${ruby(node.right)}`;
-    case "getLocal":
-      return node.name;
-    case "number":
-      return node.value.toString();
-    case "setLocal":
-      return `${node.name} = ${ruby(node.value)}`;
-    case "statements":
-      return node.stmts.map(ruby).join("\n");
+const stdlib = {
+  "@-"(recv: number) {
+    return recv * -1;
+  },
+  "+"(recv: number, arg: number) {
+    return recv + arg;
+  },
+  "-"(recv: number, arg: number) {
+    return recv - arg;
   }
 };
 
-const js = (node: AST): string => {
+function isStdLibFunc<T>(key: string): key is keyof typeof stdlib {
+  return key in stdlib;
+}
+
+const evaluate = (node: AST, locals: { [key: string]: number }): number => {
   switch (node.type) {
-    case "binary":
-      return `${js(node.left)} ${node.oper} ${js(node.right)}`;
-    case "getLocal":
-      return node.name;
-    case "number":
-      return node.value.toString();
-    case "setLocal":
-      return `const ${node.name} = ${js(node.value)}`;
-    case "statements":
-      return node.stmts.map((stmt) => `${js(stmt)};`).join("\n");
-  }
-};
-
-type LocalTable = { [key: string]: number };
-
-const evaluate = (node: AST, locals: LocalTable): number => {
-  switch (node.type) {
-    case "binary":
-      const left = evaluate(node.left, locals);
-      const right = evaluate(node.right, locals);
-
-      switch (node.oper) {
-        case "+":
-          return left + right;
-        case "-":
-          return left - right;
-        default:
-          throw new Error(`unknown operation: ${node.oper}`);
-      }
     case "getLocal":
       return locals[node.name];
     case "number":
       return node.value;
+    case "send":
+      const recv = evaluate(node.recv, locals);
+      const args = node.args.map((arg) => evaluate(arg, locals));
+      const { name } = node;
+
+      if (!isStdLibFunc(name)) {
+        throw new Error(`unknown function: ${name}`);
+      }
+
+      const func = stdlib[name];
+      if (args.length !== func.length - 1) {
+        throw new Error(`unexpected number of arguments, got ${args.length} expected ${func.length - 1}`);
+      }
+
+      return (func as any).apply(null, [recv].concat(args));
     case "setLocal":
       locals[node.name] = evaluate(node.value, locals);
       return locals[node.name];
@@ -88,23 +76,17 @@ const evaluate = (node: AST, locals: LocalTable): number => {
       node.stmts.forEach((stmt) => {
         value = evaluate(stmt, locals)
       });
-      return value === 0 ? 0 : NaN;
+      return value || (value === 0 ? 0 : NaN);
   }
 };
 
 const tree = statements([
   setLocal("foo", add(number(1), number(2))),
-  setLocal("bar", subtract(getLocal("foo"), number(3)))
+  setLocal("bar", subtract(getLocal("foo"), negative(number(3))))
 ]);
 
 console.log("DEBUG");
 console.log(debug(tree));
 
-console.log("\nRUBY");
-console.log(ruby(tree));
-
-console.log("\nJS");
-console.log(js(tree));
-
-console.log("\nEvaluation")
+console.log("\nEVAL")
 console.log(evaluate(tree, {}));
