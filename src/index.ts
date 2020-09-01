1772b157c228c6e247bad49dbba73342d1b93bdf
Kevin Deisz <kevin.deisz@gmail.com>
2020-09-01 11:22:39 -0400

    Funcs and calls

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,42 @@
+type StmtList = { type: "stmtList", stmts: AST[] };
 type AST = (
+  | { type: "call", name: string }
+  | { type: "define", name: string, stmtList: StmtList }
   | { type: "getLocal", name: string }
   | { type: "number", value: number }
   | { type: "send", recv: AST, name: string, args: AST[] }
   | { type: "setLocal", name: string, value: AST }
-  | { type: "stmtList", stmts: AST[] }
-)
+  | StmtList
+);
 
+const call = (name: string): AST => ({ type: "call", name });
+const define = (name: string, stmtList: StmtList): AST => ({ type: "define", name, stmtList });
 const getLocal = (name: string): AST => ({ type: "getLocal", name });
 const number = (value: number): AST => ({ type: "number", value });
 const send = (recv: AST, name: string, args: AST[]) => ({ type: "send" as const, recv, name, args });
 const setLocal = (name: string, value: AST): AST => ({ type: "setLocal", name, value });
-const stmtList = (stmts: AST[]): AST => ({ type: "stmtList", stmts });
+const stmtList = (stmts: AST[]): StmtList => ({ type: "stmtList", stmts });
 
 const add = (left: AST, right: AST): AST => send(left, "+", [right]);
 const negative = (value: AST): AST => send(value, "@-", []);
 const subtract = (left: AST, right: AST): AST => send(left, "-", [right]);
 
-const debug = (node: AST): string => {
+const debug = (node: AST, indent: string = ""): string => {
   switch (node.type) {
+    case "call":
+      return `${node.name}()`;
+    case "define":
+      return `function ${node.name}() {\n${debug(node.stmtList, `${indent}  `)}\n}`;
     case "getLocal":
-      return `getLocal(${node.name})`;
+      return `${indent}${node.name}`;
     case "number":
-      return `number<${node.value}>`;
+      return `${indent}<${node.value}>`;
     case "send":
-      return `${debug(node.recv)}.${node.name}(${node.args.map(debug).join(", ")})`;
+      return `${indent}${debug(node.recv)} ${node.name} ${node.args.map((arg) => debug(arg, indent)).join(", ")}`;
     case "setLocal":
-      return `setLocal(${node.name}, ${debug(node.value)})`;
+      return `${indent}${node.name} = ${debug(node.value)}`;
     case "stmtList":
-      return node.stmts.map(debug).join("\n");
+      return node.stmts.map((stmt) => debug(stmt, indent)).join("\n");
   }
 };
 
@@ -47,15 +56,25 @@ function isStdLibFunc<T>(key: string): key is keyof typeof stdlib {
   return key in stdlib;
 }
 
-const evaluate = (node: AST, locals: { [key: string]: number }): number => {
+type Context = {
+  funcs: { [key: string]: StmtList },
+  locals: { [key: string]: number }
+};
+
+const evaluate = (node: AST, context: Context): number => {
   switch (node.type) {
+    case "call":
+      return evaluate(context.funcs[node.name], { funcs: context.funcs, locals: {} });
+    case "define":
+      context.funcs[node.name] = node.stmtList;
+      return NaN;
     case "getLocal":
-      return locals[node.name];
+      return context.locals[node.name];
     case "number":
       return node.value;
     case "send":
-      const recv = evaluate(node.recv, locals);
-      const args = node.args.map((arg) => evaluate(arg, locals));
+      const recv = evaluate(node.recv, context);
+      const args = node.args.map((arg) => evaluate(arg, context));
       const { name } = node;
 
       if (!isStdLibFunc(name)) {
@@ -69,24 +88,27 @@ const evaluate = (node: AST, locals: { [key: string]: number }): number => {
 
       return (func as any).apply(null, [recv].concat(args));
     case "setLocal":
-      locals[node.name] = evaluate(node.value, locals);
-      return locals[node.name];
+      context.locals[node.name] = evaluate(node.value, context);
+      return context.locals[node.name];
     case "stmtList":
       let value = null;
       node.stmts.forEach((stmt) => {
-        value = evaluate(stmt, locals)
+        value = evaluate(stmt, context)
       });
       return value || (value === 0 ? 0 : NaN);
   }
 };
 
 const tree = stmtList([
-  setLocal("foo", add(number(1), number(2))),
-  setLocal("bar", subtract(getLocal("foo"), negative(number(3))))
+  define("func", stmtList([
+    setLocal("foo", add(number(1), number(2))),
+    setLocal("bar", subtract(getLocal("foo"), negative(number(3))))
+  ])),
+  call("func")
 ]);
 
 console.log("DEBUG");
 console.log(debug(tree));
 
 console.log("\nEVAL")
-console.log(evaluate(tree, {}));
+console.log(evaluate(tree, { funcs: {}, locals: {} }));
