43f32a0bfbf816ea6fa010cf23b707acd6f9faad
Kevin Deisz <kevin.deisz@gmail.com>
2020-09-01 10:44:10 -0400

    js -> ts

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,68 @@
-const binary = (left, oper, right) => ({ type: "binary", left, oper, right });
-const getLocal = (name) => ({ type: "getLocal", name });
-const number = (value) => ({ type: "number", value });
-const setLocal = (name, value) => ({ type: "setLocal", name, value });
-const statements = (stmts) => ({ type: "statements", statements: stmts });
+type AST = (
+  | { type: "binary", left: AST, oper: string, right: AST }
+  | { type: "getLocal", name: string }
+  | { type: "number", value: number }
+  | { type: "setLocal", name: string, value: AST }
+  | { type: "statements", stmts: AST[] }
+)
 
-const add = (left, right) => binary(left, "+", right);
-const subtract = (left, right) => binary(left, "-", right);
+const binary = (left: AST, oper: string, right: AST): AST => ({ type: "binary", left, oper, right });
+const getLocal = (name: string): AST => ({ type: "getLocal", name });
+const number = (value: number): AST => ({ type: "number", value });
+const setLocal = (name: string, value: AST): AST => ({ type: "setLocal", name, value });
+const statements = (stmts: AST[]): AST => ({ type: "statements", stmts });
 
-const debug = (node) => {
+const add = (left: AST, right: AST): AST => binary(left, "+", right);
+const subtract = (left: AST, right: AST): AST => binary(left, "-", right);
+
+const debug = (node: AST): string => {
   switch (node.type) {
     case "binary":
       return `binary(${debug(node.left)}, ${node.oper}, ${debug(node.right)})`;
     case "getLocal":
       return `getLocal(${node.name})`;
     case "number":
-      return node.value;
+      return node.value.toString();
     case "setLocal":
       return `setLocal(${node.name}, ${debug(node.value)})`;
     case "statements":
-      return node.statements.map(debug).join("\n");
+      return node.stmts.map(debug).join("\n");
   }
 };
 
-const ruby = (node) => {
+const ruby = (node: AST): string => {
   switch (node.type) {
     case "binary":
       return `${ruby(node.left)} ${node.oper} ${ruby(node.right)}`;
     case "getLocal":
       return node.name;
     case "number":
-      return node.value;
+      return node.value.toString();
     case "setLocal":
       return `${node.name} = ${ruby(node.value)}`;
     case "statements":
-      return node.statements.map(ruby).join("\n");
+      return node.stmts.map(ruby).join("\n");
   }
 };
 
-const js = (node) => {
+const js = (node: AST): string => {
   switch (node.type) {
     case "binary":
       return `${js(node.left)} ${node.oper} ${js(node.right)}`;
     case "getLocal":
       return node.name;
     case "number":
-      return node.value;
+      return node.value.toString();
     case "setLocal":
       return `const ${node.name} = ${js(node.value)}`;
     case "statements":
-      return node.statements.map((statement) => `${js(statement)};`).join("\n");
+      return node.stmts.map((stmt) => `${js(stmt)};`).join("\n");
   }
 };
 
-const evaluate = (node, locals = {}) => {
+type LocalTable = { [key: string]: number };
+
+const evaluate = (node: AST, locals: LocalTable): number => {
   switch (node.type) {
     case "binary":
       const left = evaluate(node.left, locals);
@@ -75,10 +85,10 @@ const evaluate = (node, locals = {}) => {
       return locals[node.name];
     case "statements":
       let value = null;
-      node.statements.forEach((statement) => {
-        value = evaluate(statement, locals)
+      node.stmts.forEach((stmt) => {
+        value = evaluate(stmt, locals)
       });
-      return value;
+      return value === 0 ? 0 : NaN;
   }
 };
 
@@ -97,4 +107,4 @@ console.log("\nJS");
 console.log(js(tree));
 
 console.log("\nEvaluation")
-console.log(evaluate(tree));
+console.log(evaluate(tree, {}));
