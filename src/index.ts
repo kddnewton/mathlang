e2b0331ca0a673199c3739843bd7ee7768f8bd13
Kevin Deisz <kevin.deisz@gmail.com>
2020-09-09 10:49:50 -0400

    Better types

--- src/index.ts
+++ src/index.ts
@@ -1,92 +1,130 @@
-type StmtList = { type: "stmtList", stmts: AST[] };
-type AST = (
-  | { type: "call", name: string }
-  | { type: "define", name: string, stmtList: StmtList }
-  | { type: "getLocal", name: string }
-  | { type: "number", value: number }
-  | { type: "send", recv: AST, name: string, args: AST[] }
-  | { type: "setLocal", name: string, value: AST }
-  | StmtList
-);
+// AST node types
+namespace Node {
+  export type Number = { type: "number", value: number };
 
-const call = (name: string): AST => ({ type: "call", name });
-const define = (name: string, stmtList: StmtList): AST => ({ type: "define", name, stmtList });
-const getLocal = (name: string): AST => ({ type: "getLocal", name });
-const number = (value: number): AST => ({ type: "number", value });
-const send = (recv: AST, name: string, args: AST[]) => ({ type: "send" as const, recv, name, args });
-const setLocal = (name: string, value: AST): AST => ({ type: "setLocal", name, value });
-const stmtList = (stmts: AST[]): StmtList => ({ type: "stmtList", stmts });
+  export type GetLocal = { type: "getLocal", name: string };
+  export type SetLocal = { type: "setLocal", name: string, value: Expr };
 
-const add = (left: AST, right: AST): AST => send(left, "+", [right]);
-const negative = (value: AST): AST => send(value, "@-", []);
-const subtract = (left: AST, right: AST): AST => send(left, "-", [right]);
+  export type Call = { type: "call", name: string, args: Expr[] };
+  export type Define = { type: "define", name: string, params: Param[], stmtList: StmtList };
+  export type Param = { type: "param", name: string };
 
-const debug = (node: AST, indent: string = ""): string => {
-  switch (node.type) {
-    case "call":
-      return `${node.name}()`;
-    case "define":
-      return `function ${node.name}() {\n${debug(node.stmtList, `${indent}  `)}\n}`;
-    case "getLocal":
-      return `${indent}${node.name}`;
-    case "number":
-      return `${indent}<${node.value}>`;
-    case "send":
-      return `${indent}${debug(node.recv)} ${node.name} ${node.args.map((arg) => debug(arg, indent)).join(", ")}`;
-    case "setLocal":
-      return `${indent}${node.name} = ${debug(node.value)}`;
-    case "stmtList":
-      return node.stmts.map((stmt) => debug(stmt, indent)).join("\n");
-  }
-};
+  export type StmtList = { type: "stmtList", stmts: Stmt[] };
+  export type Program = { type: "program", stmtList: StmtList };
 
+  export type Expr = Call | GetLocal | Number;
+  export type Stmt = Define | Expr | SetLocal;
+
+  export type Visitable = Stmt | StmtList | Program;
+}
+
+// AST node builders
+const number = (value: number): Node.Number => ({ type: "number", value });
+
+const getLocal = (name: string): Node.GetLocal => ({ type: "getLocal", name });
+const setLocal = (name: string, value: Node.Expr): Node.SetLocal => ({ type: "setLocal", name, value });
+
+const call = (name: string, args: Node.Expr[]): Node.Call => ({ type: "call", name, args });
+const define = (name: string, params: Node.Param[], stmtList: Node.StmtList): Node.Define => ({ type: "define", name, params, stmtList });
+const param = (name: string): Node.Param => ({ type: "param", name });
+
+const stmtList = (stmts: Node.Stmt[]): Node.StmtList => ({ type: "stmtList", stmts });
+const program = (stmtList: Node.StmtList): Node.Program => ({ type: "program", stmtList });
+
+// The standard library of this little language
 const stdlib = {
-  "@-"(recv: number) {
-    return recv * -1;
-  },
-  "+"(recv: number, arg: number) {
-    return recv + arg;
-  },
-  "-"(recv: number, arg: number) {
-    return recv - arg;
-  }
+  neg: (value: number) => value * -1,
+  add: (left: number, right: number) => left + right,
+  sub: (left: number, right: number) => left - right,
+  mul: (left: number, right: number) => left * right,
+  div: (left: number, right: number) => left / right,
+  exp: (value: number, power: number) => Math.pow(value, power)
 };
 
-function isStdLibFunc<T>(key: string): key is keyof typeof stdlib {
+function isStdLibFunc(key: string): key is keyof typeof stdlib {
   return key in stdlib;
 }
 
+// Higher-level AST node builders, could be used for further optimizations
+const unary = (name: string) => (value: Node.Expr): Node.Call => call(name, [value]);
+const binary = (name: string) => (left: Node.Expr, right: Node.Expr): Node.Call => call(name, [left, right]);
+
+const neg = unary("neg");
+const add = binary("add");
+const sub = binary("sub");
+const mul = binary("mul");
+const div = binary("div");
+const exp = binary("exp");
+
+// Output a node to readable pseudo-code
+const generate = (node: Node.Visitable, indent: string = ""): string => {
+  switch (node.type) {
+    case "number":
+      return `${indent}${node.value}`;
+    case "getLocal":
+      return `${indent}${node.name}`;
+    case "setLocal":
+      return `${indent}${node.name} = ${generate(node.value)}`;
+    case "call":
+      return `${indent}${node.name}(${node.args.map((arg) => generate(arg)).join(", ")})`;
+    case "define":
+      const params = node.params.map((param) => param.name).join(", ");
+      return `def ${node.name}(${params}) {\n${generate(node.stmtList, `${indent}  `)}\n}`;
+    case "program":
+      return generate(node.stmtList);
+    case "stmtList":
+      return node.stmts.map((stmt) => generate(stmt, indent)).join("\n");
+  }
+};
+
 type Context = {
-  funcs: { [key: string]: StmtList },
+  funcs: { [key: string]: Node.Define },
   locals: { [key: string]: number }
 };
 
-const evaluate = (node: AST, context: Context): number => {
+// Evaluate a call node within a certain context
+const evaluateCall = (node: Node.Call, context: Context) => {
+  const getArgs = (arity: number) => {
+    if (arity !== node.args.length) {
+      throw new Error(`got ${node.args.length} args, expected ${arity}`);
+    }
+
+    return node.args.map((arg) => evaluate(arg, context));
+  };
+
+  if (isStdLibFunc(node.name)) {
+    const func = stdlib[node.name];
+    return (func as any)(...getArgs(func.length));
+  }
+
+  if (Object.prototype.hasOwnProperty.call(context.funcs, node.name)) {
+    const define = context.funcs[node.name];
+    const locals: { [key: string]: number } = {};
+
+    getArgs(define.params.length).forEach((arg, index) => {
+      locals[define.params[index].name] = arg;
+    });
+
+    return evaluate(define.stmtList, { funcs: context.funcs, locals });
+  }
+
+  throw new Error(`unknown function: ${name}`);
+};
+
+// Evaluate a visitable AST node within a certain context
+const evaluate = (node: Node.Visitable, context: Context): number => {
   switch (node.type) {
     case "call":
-      return evaluate(context.funcs[node.name], { funcs: context.funcs, locals: {} });
+      return evaluateCall(node, context);
     case "define":
-      context.funcs[node.name] = node.stmtList;
+      context.funcs[node.name] = node;
       return NaN;
     case "getLocal":
       return context.locals[node.name];
     case "number":
       return node.value;
-    case "send":
-      const recv = evaluate(node.recv, context);
-      const args = node.args.map((arg) => evaluate(arg, context));
-      const { name } = node;
-
-      if (!isStdLibFunc(name)) {
-        throw new Error(`unknown function: ${name}`);
-      }
-
-      const func = stdlib[name];
-      if (args.length !== func.length - 1) {
-        throw new Error(`unexpected number of arguments, got ${args.length} expected ${func.length - 1}`);
-      }
-
-      return (func as any).apply(null, [recv].concat(args));
+    case "program":
+      return evaluate(node.stmtList, context);
     case "setLocal":
       context.locals[node.name] = evaluate(node.value, context);
       return context.locals[node.name];
@@ -99,16 +137,18 @@ const evaluate = (node: AST, context: Context): number => {
   }
 };
 
-const tree = stmtList([
-  define("func", stmtList([
-    setLocal("foo", add(number(1), number(2))),
-    setLocal("bar", subtract(getLocal("foo"), negative(number(3))))
+// Build out an example tree
+const tree = program(stmtList([
+  define("func", [param("foo")], stmtList([
+    setLocal("bar", add(getLocal("foo"), exp(number(2), number(2)))),
+    setLocal("baz", sub(getLocal("bar"), neg(number(3)))),
+    add(getLocal("baz"), number(2))
   ])),
-  call("func")
-]);
+  call("func", [number(1)])
+]));
 
-console.log("DEBUG");
-console.log(debug(tree));
+console.log("GENERATE:");
+console.log(generate(tree));
 
-console.log("\nEVAL")
+console.log("\nEVALUATE:");
 console.log(evaluate(tree, { funcs: {}, locals: {} }));
