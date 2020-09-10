1c694f97117ec2fcd07125202ecad7bb739cd9f5
Kevin Deisz <kevin.deisz@gmail.com>
2020-09-10 12:13:11 -0400

    Simplify test

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,7 @@ import generator from "./generator";
 import parser from "./parser";
 import tokenizer from "./tokenizer";
 
-const input =
-`f(a) {
-  a + 1
-}
-a = f(2)
-a + 1
-`;
+const input = "add(1, 2)";
 
 const tree = parser(tokenizer(input));
 console.log(generator(tree));
