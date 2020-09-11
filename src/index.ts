d814c658b3bbd51a9e6068b1ee79983cef88acb5
Kevin Deisz <kevin.deisz@gmail.com>
2020-09-11 19:09:04 -0400

    Add optimizer

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import compiler from "./compiler";
 import formatter from "./formatter";
 import interpreter from "./interpreter";
 import machine from "./machine";
+import optimizer from "./optimizer";
 import parser from "./parser";
 import tokenizer from "./tokenizer";
 
 export const evaluate = (source: string) => (
-  machine(compiler(parser(tokenizer(source))))
+  machine(compiler(optimizer(parser(tokenizer(source)))))
 );
 
 export const format = (source: string) => (
